b7c2d8883532285a252f589038a515a709944c0c
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-10 12:04:30 +0000

    Exercise 9

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string question, response;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+            string question = opcao switch 
+            {
+                "How are you?"=> " I’m fine , thank you ".
+                "What's your name?"=> "Gran'pa".
+                "What's your mission?"=> "Destroy mankind!".
+                _ => "You got me, I'm not THAT smart!".
+            }
+        }
+    }
+}   
